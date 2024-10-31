f71d1b3304d5c94785381f232119f63ca8159baf
MichaelGarciaJ <mgarcia271476@gmail.com>
2024-10-31 11:26:38 +0100

    A10 PT2

--- UT1/1.10.FormularioEmpleado/1.10.FormularioEmpleado/MainWindow.xaml.cs
+++ UT1/1.10.FormularioEmpleado/1.10.FormularioEmpleado/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,12 +28,299 @@ namespace _1._10.FormularioEmpleado
 
         private void txtNombre_GotFocus(object sender, RoutedEventArgs e)
         {
-
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
         }
 
         private void txtNombre_LostFocus(object sender, RoutedEventArgs e)
         {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtApellidos_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtApellidos_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtEmail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtEmail_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtTelefono_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtTelefono_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtDni_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtDni_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void btnCargarFoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.png)|*.jpg;*.png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MessageBox.Show("Foto cargada: " + openFileDialog.FileName);
+            }
+        }
+
+        private void txtDireccion_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtDireccion_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtCiudad_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtCiudad_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtProvincia_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtProvincia_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtCodigoPostal_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtCodigoPostal_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtPais_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtPais_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtEnlaceRedSocial_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtEnlaceRedSocial_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void txtDescricpcion_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == textBox.Tag.ToString())
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void txtDescricpcion_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = textBox.Tag.ToString();
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void btnGuardar(object sender, RoutedEventArgs e)
+        {
+            if (ValidateForm())
+            {
+                MessageBox.Show("Form is valid. Saving data...");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all required fields.");
+            }
+        }
+
+        private void btnCancelar(object sender, RoutedEventArgs e)
+        {
+            txtNombre.Text = "";
+            txtApellidos.Text = "";
+            txtEmail.Text = "";
+            txtTelefono.Text = "";
+            txtDni.Text = "";
+            txtDireccion.Text = "Direccion";
+            txtCiudad.Text = "Ciudad";
+            txtProvincia.Text = "Provincia";
+            txtCodigoPostal.Text = "CodigoPostal";
+            txtPais.Text = "Pais";
+            txtEnlaceRedSocial.Text = "";
+            txtDescricpcion.Text = "";
+            cbBoxRol.SelectedIndex = -1;
+            dtPickerFechaNacimiento.SelectedDate = null;
+        }
+
+        private void dataGridEmpleados_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
+
+        private bool ValidateForm()
+        {
+            return !string.IsNullOrWhiteSpace(txtNombre.Text) && txtNombre.Text != "Nombre" &&
+                   !string.IsNullOrWhiteSpace(txtEmail.Text) && txtEmail.Text != "Email" &&
+                   !string.IsNullOrWhiteSpace(txtTelefono.Text) && txtTelefono.Text != "Telefono" &&
+                   dtPickerFechaNacimiento.SelectedDate != null &&
+                   !string.IsNullOrWhiteSpace(txtDni.Text) && txtDni.Text != "Dni";
+        }
+
     }
 }
